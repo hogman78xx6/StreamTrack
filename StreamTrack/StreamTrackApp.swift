ba5ddeddac49565f4c137386dddcdb47f9c49869
Michael Knych <mmknych@yahoo.com>
2024-12-03 17:45:15 -0600

    Initial Commit

---
+++ StreamTrack/StreamTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamTrackApp.swift
+//  StreamTrack
+//
+//  Created by Michael Knych on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct StreamTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
