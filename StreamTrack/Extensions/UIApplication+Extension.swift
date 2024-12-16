924ad13f5473bccb9aaf481651a19f5139a1aa87
Michael Knych <mmknych@yahoo.com>
2024-12-15 18:35:35 -0600

    version 1.1. changes as of 12/15/24

---
+++ StreamTrack/Extensions/UIApplication+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIApplication+Extension.swift
+//  StreamTrack
+//
+//  Created by Michael Knych on 12/14/24.
+//
+
+import Foundation
---
+++ StreamTrack/Views/ShowLivePosterView.swift
@@ -0,0 +1,18 @@
+//
+//  ShowLivePosterView.swift
+//  StreamTrack
+//
+//  Created by Michael Knych on 12/15/24.
+//
+
+import SwiftUI
+
+struct ShowLivePosterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowLivePosterView()
+}
